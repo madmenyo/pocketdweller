a86271efc8bda94dcf7bd7ce7cdcf48f794bb391
madmenyo <b4xs1x3nmhil>
2020-09-25 14:30:09 +0200

    Clean libGDX project

---
+++ android/src/com/buckriderstudio/pocketdweller/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.buckriderstudio.pocketdweller;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.buckriderstudio.pocketdweller.PocketDweller;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PocketDweller(), config);
+	}
+}
---
+++ desktop/src/com/buckriderstudio/pocketdweller/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.buckriderstudio.pocketdweller.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.buckriderstudio.pocketdweller.PocketDweller;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PocketDweller(), config);
+	}
+}
