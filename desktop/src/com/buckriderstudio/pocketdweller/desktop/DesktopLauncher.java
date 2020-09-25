938b7041149e5b2757a6903353500c2bb213f1f8
madmenyo <b4xs1x3nmhil>
2020-09-25 14:41:55 +0200

    Added images Texturepacking functionality Ignored processed images in assets folder

--- desktop/src/com/buckriderstudio/pocketdweller/desktop/DesktopLauncher.java
+++ desktop/src/com/buckriderstudio/pocketdweller/desktop/DesktopLauncher.java
@@ -2,11 +2,27 @@ package com.buckriderstudio.pocketdweller.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.buckriderstudio.pocketdweller.PocketDweller;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		packImages();
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new PocketDweller(), config);
+	}
+
+	private static void packImages() {
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.fast = true;
+		settings.combineSubdirectories = true;
+		settings.flattenPaths = true;
+
+		String input = "../../images/";
+		String output = "tilesets";
+		String filename = "dungeon.atlas";
+
+		TexturePacker.process(settings, input, output, filename);
 	}
 }
